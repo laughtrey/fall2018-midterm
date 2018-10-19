9a69544db16fe22422ee51e5f7fc6ed727325db6
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-18 17:23:28 -0700

    Added Javadoc and removed the sentence and character outputs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 /**
-Description:
-Filename:
-Author:
-Date:f
+Description: This is the mid-term composed of several functions. It will analyze
+a sentence and return how many of the selected characters there are.
+Filename: main.cpp
+Author: Raymond Laughrey
+Email: raymonl4963@student.vvc.edu
+Date: 10/18/2018, 5:15:38 PM
 */
 #include<iostream>
 #include<string>
@@ -16,13 +18,19 @@ int char_count(string, char);
 
 int main()
 {
-   string str = get_string("Please enter the sentence: ");
+   string str = get_string("Please enter a sentence you would like to analyze: ");
    char c = get_char("Please enter the character you would like to count: ");
-   cout << str << endl;
-   cout << c << endl;
+   //cout << str << endl;
+   //cout << c << endl;
    cout << "Your sentance has " << char_count(str, c) << " of the characters you selected."<< endl;
    return 0;
 }
+/*
+Recieves the sentence string and converts it to lowercase
+@param str string input
+@param prompt prints the prompt to request input
+@return returns the lowercase sentence
+*/
 string get_string(string prompt)
 {
    string str;
@@ -34,6 +42,12 @@ string get_string(string prompt)
    }
    return str;
 }
+/*
+Recieves the letter to count in the string
+@param c the selected character
+@param prompt the prompt asking the user for input
+@return returns the selected character
+*/
 char get_char(string prompt)
 {
    char c;
@@ -41,6 +55,11 @@ char get_char(string prompt)
    cin >> c;
    return c;
 }
+/*
+Counts the amount of specified characters in the string
+@param count placeholder int for the amount
+@return returns the counted character
+*/
 int char_count(string str, char c)
 {
    int count = 0;
